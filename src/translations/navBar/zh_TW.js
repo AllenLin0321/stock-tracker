8cd853274d406eef9561f3d562a12dae49e7b299
Allen <allen0321.lin@ehsn.com.tw>
2020-09-14 11:38:06 +0800

    更新導覽列 Title

--- src/translations/navBar/zh_TW.js
+++ src/translations/navBar/zh_TW.js
@@ -1,7 +1,7 @@
 export default {
   'navBar.list': '觀察名單',
-  'navBar.portfolio': '資產組合',
-  'navBar.chart': '圓餅圖',
+  'navBar.portfolio': '投資組合',
+  'navBar.chart': '資產配置',
   'navBar.rebalance': '再平衡',
   'navBar.donate': '贊助',
 };
