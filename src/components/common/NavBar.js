032572eac4a343e6e7ca2fba4bc2cf19c28bcef6
Allen <allen0321.lin@ehsn.com.tw>
2020-09-14 11:14:37 +0800

    優化 NavBar

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Tabs } from 'antd';
+import { Tabs, Tooltip } from 'antd';
 import {
   LineChartOutlined,
   UserOutlined,
@@ -9,67 +9,64 @@ import {
 } from '@ant-design/icons';
 import { injectIntl } from 'react-intl';
 
-class NavBar extends React.Component {
-  state = {
-    tabs: [
-      {
-        icon: <LineChartOutlined />,
-        key: 'list',
-      },
-      {
-        icon: <UnorderedListOutlined />,
-        key: 'portfolio',
-      },
-      {
-        icon: <UserOutlined />,
-        key: 'chart',
-      },
-      {
-        icon: <CalculatorOutlined />,
-        key: 'rebalance',
-      },
-      {
-        icon: <CoffeeOutlined />,
-        key: 'donate',
-      },
-    ],
-  };
+const tabs = [
+  {
+    icon: <LineChartOutlined />,
+    key: 'list',
+  },
+  {
+    icon: <UnorderedListOutlined />,
+    key: 'portfolio',
+  },
+  {
+    icon: <UserOutlined />,
+    key: 'chart',
+  },
+  {
+    icon: <CalculatorOutlined />,
+    key: 'rebalance',
+  },
+  {
+    icon: <CoffeeOutlined />,
+    key: 'donate',
+  },
+];
 
-  renderTabPane = () => {
-    if (this.state.tabs.length === 0) return;
-
-    const TabPaneJsx = this.state.tabs.map(tab => {
-      return (
-        <Tabs.TabPane
-          tab={
-            <span
-              title={this.props.intl.formatMessage({
-                id: `navBar.${tab.key}`,
-              })}
-            >
-              {tab.icon}
-            </span>
-          }
-          key={tab.key}
-        ></Tabs.TabPane>
-      );
-    });
-    return TabPaneJsx;
-  };
-
-  render() {
+const renderTabPane = props => {
+  const TabPaneJsx = tabs.map(tab => {
     return (
-      <div className="narBar__wrapper">
-        <Tabs
-          type="card"
-          defaultActiveKey="list"
-          onChange={key => this.props.history.push(`/${key}`)}
-        >
-          {this.renderTabPane()}
-        </Tabs>
-      </div>
+      <Tabs.TabPane
+        tab={
+          <Tooltip
+            placement="bottom"
+            title={props.intl.formatMessage({
+              id: `navBar.${tab.key}`,
+            })}
+          >
+            {tab.icon}
+          </Tooltip>
+        }
+        key={tab.key}
+      ></Tabs.TabPane>
     );
-  }
-}
+  });
+  return TabPaneJsx;
+};
+
+const NavBar = props => {
+  const tabsConfig = {
+    type: 'card',
+    defaultActiveKey: 'list',
+    size: 'large',
+    tabBarGutter: 2,
+    onChange: key => props.history.push(`/${key}`),
+  };
+
+  return (
+    <div className="narBar__wrapper">
+      <Tabs {...tabsConfig}>{renderTabPane(props)}</Tabs>
+    </div>
+  );
+};
 
 export default injectIntl(NavBar);
